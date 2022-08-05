2d7eef6025b3fcdde7f8d5210d05339a466bc0b1
AJ-1009 <108213911+AJ-1009@users.noreply.github.com>
2022-08-05 18:22:24 +0530

    Update card.js

--- src/js/card.js
+++ src/js/card.js
@@ -5,7 +5,7 @@ function card(props){
             <h4 className="position">NIRF Ranking : {props.rank} </h4>
             <img src={props.img} className='img' alt="" />
             <h4 className='name'>{props.Name}</h4>
-            <a href={props.links}className='links' target='_blank'> Visit the website </a>
+            <a href={props.link}className='links' target='_blank'> Visit the website </a>
         </section>
     )
 }
